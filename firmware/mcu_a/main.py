5ac943d6699aab055ea356adbaa6a9eb0600f9c3
HenryLi-0 <bxsci8266@gmail.com>
2026-09-12 01:14:27 -0400

    quick basic firmware and small updates!

--- firmware/mcu_a/main.py
+++ firmware/mcu_a/main.py
@@ -1,19 +1,109 @@
 import board
 import digitalio
+import busio
 import time
+import os
 
 import storage
-import adafruit_sdcard
+import sdcardio
+
+import displayio
+import terminalio
+import adafruit_displayio_ssd1306 #??? idk maybe
+
+import audiomp3
+import audiopwmio
 
 # ??? still need to understand these docs
 
-led = digitalio.DigitalInOut(board.GP25)
+'''DOUBLE CHECK THESE PINS'''
+class Constants:
+    BUTTONS = [board.GP4, board.GP5, board.GP6]
+
+    class GPU_SPI:
+        SCK = board.GP0
+        MOSI = board.GP1
+        MISO = board.GP2
+        CS = board.GP3
+
+    class GPU_UART:
+        TX = board.GP16
+        RX = board.GP17
+
+    class SD:
+        SCK = board.GP11
+        MOSI = board.GP12
+        MISO = board.GP13
+        CS = board.GP14
+
+        DIR = "/sd" # ???
+        EXTENSIONS = [".mp3", ".wav"]
+
+    class OLED:
+        SCL = board.GP18
+        SDA = board.GP19
+
+    class LEDS:
+        RGB = board.GP23
+        BOARD = board.GP24
+        BOOT = board.GP25
+
+    class AUDIO:
+        pass
+
+led = digitalio.DigitalInOut(Constants.LEDS.BOOT)
 led.direction = digitalio.Direction.OUTPUT
+led.value = True
 
-while True:
-    led.value = True
-    time.sleep(0.5)
-    led.value = False
-    time.sleep(0.5)
+class MCU_A:
+    def __init__(self):
+        # self.setupSPI()
+        self.setupUART()
 
-# so uh, i cant actually write any of this without the actual thing
+    # setup comms (in theory)
+    def setupUART(self):
+        self.uart = busio.UART(
+            Constants.GPU_UART.TX,
+            Constants.GPU_UART.RX # where does baudrate come in
+        )
+    def setupSPI(self):
+        self.spi = busio.SPI(
+            Constants.GPU_SPI.SCK,
+            Constants.GPU_SPI.MOSI,
+            Constants.GPU_SPI.MISO
+        )
+    def send(self):
+        if self.uart is None:
+            self.setupUART()
+        # ???
+
+    def setupSD(self):
+        self.sdSPI = busio.SPI(
+            Constants.SD.SCK,
+            Constants.SD.MOSI,
+            Constants.SD.MISO
+        )
+        self.sd = sdcardio.SDCard(
+            self.sdSPI,
+            Constants.SD.CS # where does baudrate come in
+        )
+
+        '''TODO mount and read the sd card???'''
+
+
+    def setupPlay(self):
+        self.pause = False
+        self.songID = 0
+        # pick first song idk
+
+    def play(self):
+        self.pause = False
+
+    def stop(self):
+        self.pause = False
+        # ???
+    
+
+mcu = MCU_A()
+
+# some update loop for the user stuff
--- firmware/mcu_b/main.py
+++ firmware/mcu_b/main.py
@@ -2,15 +2,74 @@
 
 import board
 import digitalio
+import busio
 import time
 
-led = digitalio.DigitalInOut(board.GP25)
+import displayio
+
+'''DOUBLE CHECK THESE PINS'''
+class Constants:
+    class CPU_SPI:
+        SCK = board.GP0
+        MOSI = board.GP2
+        MISO = board.GP1
+        CS = board.GP3
+
+    class CPU_UART:
+        TX = board.GP17
+        RX = board.GP16
+
+    class TFT:
+        DC = board.GP8
+        CS = board.GP9
+        SCK = board.GP10
+        MOSI = board.GP11
+        RST = board.GP12
+        BL = board.GP13
+
+    class LEDS:
+        RGB = board.GP23
+        BOARD = board.GP24
+        BOOT = board.GP25
+
+led = digitalio.DigitalInOut(Constants.LEDS.BOOT)
 led.direction = digitalio.Direction.OUTPUT
+led.value = True
 
-while True:
-    led.value = True
-    time.sleep(0.5)
-    led.value = False
-    time.sleep(0.5)
+class MCU_B:
+    def __init__(self):
+        # self.setupSPI()
+        self.setupUART()
 
-# so uh, i cant actually write any of this without the actual thing
+        self.setupDisplay()
+
+    # setup comms (in theory)
+    def setupUART(self):
+        self.uart = busio.UART(
+            Constants.CPU_UART.TX,
+            Constants.CPU_UART.RX # where does baudrate come in
+        )
+    def setupSPI(self):
+        self.spi = busio.SPI(
+            Constants.CPU_SPI.SCK,
+            Constants.CPU_SPI.MOSI,
+            Constants.CPU_SPI.MISO
+        )
+    def send(self):
+        if self.uart is None:
+            self.setupUART()
+        # ???
+
+    def setupDisplay(self):
+        self.displaySPI = busio.SPI(
+            Constants.TFT.SCK,
+            MOSI = Constants.TFT.MOSI
+        )
+        self.display = displayio.FourWire( #??? idk if this is correct
+            self.displaySPI,
+            command = Constants.TFT.DC,
+            chip_select = Constants.TFT.CS,
+            reset = Constants.TFT.RST,
+        )
+
+mcu = MCU_B()
